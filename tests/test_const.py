7075779cc7e97733248bfb0fd3cbeca3336ea3cc
muchu hsu <muchu1983@debian.humanimality.xyz>
2026-05-16 15:21:20 +0800

    create yyp code map

--- tests/test_const.py
+++ tests/test_const.py
@@ -1,4 +1,5 @@
 import unittest
+import random
 from penta_lemon.const import Const
 
 class TestConst(unittest.TestCase):
@@ -8,7 +9,14 @@ class TestConst(unittest.TestCase):
 
     def test_const(self):
         self.assertEqual(len(Const.YypCode), 64+8)
-        self.assertEqual(Const.YypCode.水火既濟.value, Const.YypCode.火.value + Const.YypCode.水.value)
+        self.assertEqual(Const.YypCode._121212.value[2], Const.YypCode.火.value + Const.YypCode.水.value)
 
+    def test_yyp_code_map(self):
+        self.assertEqual(len(Const.YypCodeMap), 64)
+        yyp_code_key = "".join(random.choice("12") for _ in range(6))
+        self.assertEqual(Const.YypCodeMap[yyp_code_key].value[0], yyp_code_key)
+        self.assertIsNotNone(Const.YypCodeMap[yyp_code_key].value[1])
+        self.assertIsNotNone(Const.YypCodeMap[yyp_code_key].value[2])
+        
 if __name__ == "__main__":
     unittest.main()
