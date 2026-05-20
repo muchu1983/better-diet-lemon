274e03364bb0c93e403054e8c778755f178b6b9a
muchu hsu <muchu1983@debian.humanimality.xyz>
2026-05-20 08:34:19 +0800

    add time driven xxxv generator py and test

---
+++ tests/xxxv_generator/test_time_driven.py
@@ -0,0 +1,15 @@
+import unittest
+from penta_lemon.lemon import Lemon
+from penta_lemon.xxxv_generator.time_driven import *
+
+class TestOnpm(unittest.TestCase):
+
+    def setUp(self):
+        self.lemon = Lemon()
+
+    def test_time_driven_xxxv_generator(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
