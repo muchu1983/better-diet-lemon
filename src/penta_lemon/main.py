96d201f4bbeb72668f25adddd6ddac4c740600c9
muchu hsu <muchu1983@debian.humanimality.xyz>
2026-06-01 16:52:27 +0800

    add 3rd widget

--- src/penta_lemon/main.py
+++ src/penta_lemon/main.py
@@ -5,19 +5,27 @@ from penta_lemon.gui.lemon_widget import LemonWidget
 
 def main():
     app = QtWidgets.QApplication([])
-    #第0位元
+
+    #第0位元widget
     widget0 = LemonWidget()
     timeDrivenXxxvGen = TimeDrivenXxxvGen()
     timer = QtCore.QTimer()
     timer.timeout.connect(lambda:widget0.updateText(food=timeDrivenXxxvGen.getXxxv()))
-    timer.start(300)
-    widget0.resize(400, 50)
-    widget0.show()
-    #第1位元
+    timer.start(1000)
+    #第1位元widget
     widget1 = LemonWidget()
     widget0.singalToNextLemonWidget.connect(lambda:widget1.updateText(food=widget0.lemon.getCarryXxxv()))
-    widget1.resize(400, 50)
-    widget1.show()
+    #第2位元widget
+    widget2 = LemonWidget()
+    widget1.singalToNextLemonWidget.connect(lambda:widget2.updateText(food=widget1.lemon.getCarryXxxv()))
+    #main window
+    mainWin = QtWidgets.QWidget()
+    mainWin.resize(1200, 50)
+    layout = QtWidgets.QHBoxLayout(mainWin)
+    layout.addWidget(widget0)
+    layout.addWidget(widget1)
+    layout.addWidget(widget2)
+    mainWin.show()
     sys.exit(app.exec())
 
 if __name__ == "__main__":
