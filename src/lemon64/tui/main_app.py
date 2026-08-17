849cd262cef16ad49438663251b43bccf42d4a16
muchu hsu <muchu1983@debian.humanimality.xyz>
2026-08-18 07:58:53 +0800

    add button demo

--- src/lemon64/tui/main_app.py
+++ src/lemon64/tui/main_app.py
@@ -1,9 +1,14 @@
 from textual.app import App
-from textual.widgets import Label
+from textual.widgets import Label, Button
 
 class Hello(App):
 	def compose(self):
 		yield Label("Hello lemon64")
+		yield Button("按我", id="btn")
+
+	def on_button_pressed(self, event:Button.Pressed):
+		if event.button.id == "btn":
+			self.notify("Hello lemon64")
 
 if __name__ == "__main__":
 	app = Hello(css_path="./main_app.tcss", watch_css=True)
