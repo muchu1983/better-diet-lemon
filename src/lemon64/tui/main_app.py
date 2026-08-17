d088627174c83e373df253807f0ee9ee45105233
muchu hsu <muchu1983@debian.humanimality.xyz>
2026-08-18 07:06:17 +0800

    add tui hello world code

---
+++ src/lemon64/tui/main_app.py
@@ -0,0 +1,10 @@
+from textual.app import App
+from textual.widgets import Label
+
+class Hello(App):
+	def compose(self):
+		yield Label("Hello lemon64")
+
+if __name__ == "__main__":
+	app = Hello(css_path="./main_app.css", watch_css=True)
+	app.run()
