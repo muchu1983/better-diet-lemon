6e3dc80f8bccd3aa97c6a94699c2752fba1c1f90
muchu hsu <muchu1983@debian.humanimality.xyz>
2026-08-20 11:34:05 +0800

    building main_app.py

--- src/lemon64/tui/main_app.py
+++ src/lemon64/tui/main_app.py
@@ -1,15 +1,37 @@
 from textual.app import App, ComposeResult
-from textual.containers import Horizontal, Vertical
+from textual.screen import Screen
+from textual.containers import Horizontal, Vertical, HorizontalGroup, VerticalGroup, VerticalScroll, HorizontalScroll
 from textual.widgets import Header, Footer, Static, Label, Button, Input
+from textual.reactive import reactive
 
-class MainApp(App):
+class ListScreen(Screen):
+	TITLE = "lemon64"
+	SUB_TITLE = "list"
+	
 	def compose(self) -> ComposeResult:
 		yield Header()
 		yield Footer()
+		yield Static("柠檬64 App", id="my-static")
 
-	def on_button_pressed(self, event:Button.Pressed):
-		pass
+class MapScreen(Screen):
+	TITLE = "lemon64"
+	SUB_TITLE = "map"
+	
+	def compose(self) -> ComposeResult:
+		yield Header()
+		yield Footer()
+		yield Static("柠檬64 App", id="my-static")
+
+class MainApp(App):
+	CSS_PATH = "main_app.tcss"
+	BINDINGS = [("l", "switch_mode('list')", "列表"),
+				("m", "switch_mode('map')", "地图")]
+	MODES = {"list": ListScreen,
+			 "map": MapScreen}
+	
+	def on_mount(self):
+		self.switch_mode("list")
 
 if __name__ == "__main__":
-	app = MainApp(css_path="./main_app.tcss", watch_css=True)
+	app = MainApp(watch_css=True)
 	app.run()
