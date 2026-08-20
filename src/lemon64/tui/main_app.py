12c9176bacb453fa13aab878070b0a45363f54af
muchu hsu <muchu1983@debian.humanimality.xyz>
2026-08-20 11:45:53 +0800

    adjust code

--- src/lemon64/tui/main_app.py
+++ src/lemon64/tui/main_app.py
@@ -7,20 +7,18 @@ from textual.reactive import reactive
 class ListScreen(Screen):
 	TITLE = "lemon64"
 	SUB_TITLE = "list"
-	
 	def compose(self) -> ComposeResult:
 		yield Header()
 		yield Footer()
-		yield Static("柠檬64 App", id="my-static")
+		yield Static("柠檬64 列表", id="my-static")
 
 class MapScreen(Screen):
 	TITLE = "lemon64"
 	SUB_TITLE = "map"
-	
 	def compose(self) -> ComposeResult:
 		yield Header()
 		yield Footer()
-		yield Static("柠檬64 App", id="my-static")
+		yield Static("柠檬64 地图", id="my-static")
 
 class MainApp(App):
 	CSS_PATH = "main_app.tcss"
@@ -28,7 +26,6 @@ class MainApp(App):
 				("m", "switch_mode('map')", "地图")]
 	MODES = {"list": ListScreen,
 			 "map": MapScreen}
-	
 	def on_mount(self):
 		self.switch_mode("list")
 
