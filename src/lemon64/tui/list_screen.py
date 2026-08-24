8b68e570ebf126ef3095c1408da05a4f071c955e
Muchu Hsu <muchu1983@gmail.com>
2026-08-24 12:41:19 +0800

    list-screen ui design

--- src/lemon64/tui/list_screen.py
+++ src/lemon64/tui/list_screen.py
@@ -4,6 +4,16 @@ from textual.containers import Horizontal, Vertical, HorizontalGroup, VerticalGr
 from textual.widgets import Header, Footer, Static, Label, Button, Input, Rule
 from textual.reactive import reactive
 
+class PersonStatic(Static):
+	def __init__(self, text:str, **kwargs):
+		super().__init__(**kwargs)
+		self.person_name = text
+
+	def compose(self) -> ComposeResult:
+		yield Label(self.person_name)
+		yield Label("人格")
+		yield Button("发布", variant="primary", id="publish-to-map")
+
 class ListScreen(Screen):
 	TITLE = "lemon64"
 	SUB_TITLE = "list"
@@ -15,7 +25,7 @@ class ListScreen(Screen):
 		with Horizontal():
 			yield HorizontalScroll(
 				VerticalScroll(
-					Static("本人", classes="left-panel"),
+					PersonStatic("本人", id="myself"),
 					Rule.horizontal(),
 					Static("他人1", classes="left-panel"),
 					Static("他人2", classes="left-panel"),
