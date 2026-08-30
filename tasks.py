92470daeb72ccd0e29d467c937a15e9a9ea4c7bc
Muchu Hsu <muchu1983@gmail.com>
2026-08-31 03:40:39 +0800

    add invoke

---
+++ tasks.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from invoke import task
+import shutil
+
+@task
+def build_win(ctx):
+    # 打包exe
+    ctx.run("pyinstaller --onefile src/lemon64/main.py")
+    # 複製exe到根目錄下
+    exe_source_file = "./dist/main.exe"
+    exe_target_file = "./lemon64.exe"
+    shutil.copy2(exe_source_file, exe_target_file)
+    # 刪除打包產生的build及dist資料夾
+    build_folder_path = "./build"
+    shutil.rmtree(build_folder_path)
+    dist_folder_path = "./dist"
+    shutil.rmtree(dist_folder_path)
+    print("pyinstaller build successfully!")
+
+@task
+def build_linux(ctx):
+    # 打包exe
+    ctx.run("pyinstaller --onefile src/lemon64/main.py --add-data src/lemon64/tui/*.tcss:src/lemon64/tui/")
+    # 複製bin到根目錄下
+    bin_source_file = "./dist/main"
+    bin_target_file = "./lemon64.bin"
+    shutil.copy2(bin_source_file, bin_target_file)
+    # 刪除打包產生的build及dist資料夾
+    build_folder_path = "./build"
+    shutil.rmtree(build_folder_path)
+    dist_folder_path = "./dist"
+    shutil.rmtree(dist_folder_path)
+    print("pyinstaller build successfully!")
